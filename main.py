00b8426d15a5312cb73c6e965a5372978c5b05d2
WorkingRobot <aleks.margarian1@gmail.com>
2016-09-21 14:18:42 -0700

    ''

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import win32gui, win32ui, win32con
 from win32api import GetSystemMetrics
+from time import time
 
 windowname = "missingno"
 w, h = GetSystemMetrics(0), GetSystemMetrics(1)
-bmpfilename = "magic.bmp"
+images = 10
 
-hwnd = win32gui.FindWindow(None, windowname)
-wDC = win32gui.GetWindowDC(hwnd)
-dcObj=win32ui.CreateDCFromHandle(wDC)
-cDC=dcObj.CreateCompatibleDC()
-dataBitMap = win32ui.CreateBitmap()
-dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
-cDC.SelectObject(dataBitMap)
-cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
-dataBitMap.SaveBitmapFile(cDC, bmpfilename)
-# Free Resources
-dcObj.DeleteDC()
-cDC.DeleteDC()
-win32gui.ReleaseDC(hwnd, wDC)
-win32gui.DeleteObject(dataBitMap.GetHandle())
+for i in range(images):
+    hwnd = win32gui.FindWindow(None, windowname)
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj=win32ui.CreateDCFromHandle(wDC)
+    cDC=dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
+    dataBitMap.SaveBitmapFile(cDC, str(time())+".bmp")
+    # Free Resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
