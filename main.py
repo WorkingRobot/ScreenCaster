1954b9c6ab9cc9b81fe33f06d9d064e0e426f718
PixelatedNetowrk <DERPMYFACELKLOL@mail.com>
2016-09-20 17:44:04 -0700

    Legal and Basic Stuff

---
+++ main.py
@@ -0,0 +1,20 @@
+import win32gui, win32ui, win32con
+
+windowname = "missingno"
+w, h = 9000, 9000
+bmpfilename = "magic.bmp"
+
+hwnd = win32gui.FindWindow(None, windowname)
+wDC = win32gui.GetWindowDC(hwnd)
+dcObj=win32ui.CreateDCFromHandle(wDC)
+cDC=dcObj.CreateCompatibleDC()
+dataBitMap = win32ui.CreateBitmap()
+dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+cDC.SelectObject(dataBitMap)
+cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
+dataBitMap.SaveBitmapFile(cDC, bmpfilename)
+# Free Resources
+dcObj.DeleteDC()
+cDC.DeleteDC()
+win32gui.ReleaseDC(hwnd, wDC)
+win32gui.DeleteObject(dataBitMap.GetHandle())
