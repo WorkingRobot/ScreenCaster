eae9ee5ceb2736f3925f4c911e7f3fff7b000ddd
WorkingRobot <aleks.margarian1@gmail.com>
2016-09-21 21:00:20 -0700

    Basis of Recorder without Sound

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
-import win32gui, win32ui, win32con
+import win32gui, win32ui, win32con, os
 from win32api import GetSystemMetrics
 from time import time
 
 windowname = "missingno"
 w, h = GetSystemMetrics(0), GetSystemMetrics(1)
-images = 10
+images = 100
+preimage = 0
+av_sum = 0
+average = 0
+average_index = 0
 
+def getaverage(num):
+    global average_index, average, av_sum
+    average_index += 1;
+    av_sum += num
+    average = av_sum / average_index
+
+starttime = 0
+
+hwnd = win32gui.FindWindow(None, windowname)
+wDC = win32gui.GetWindowDC(hwnd)
+dcObj=win32ui.CreateDCFromHandle(wDC)
+starttime = time()
 for i in range(images):
-    hwnd = win32gui.FindWindow(None, windowname)
-    wDC = win32gui.GetWindowDC(hwnd)
-    dcObj=win32ui.CreateDCFromHandle(wDC)
     cDC=dcObj.CreateCompatibleDC()
     dataBitMap = win32ui.CreateBitmap()
     dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
     cDC.SelectObject(dataBitMap)
     cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
-    dataBitMap.SaveBitmapFile(cDC, str(time())+".bmp")
-    # Free Resources
-    dcObj.DeleteDC()
-    cDC.DeleteDC()
-    win32gui.ReleaseDC(hwnd, wDC)
+    inf = win32gui.GetCursorInfo()
+    cDC.DrawIcon(inf[2],inf[1])
+    dataBitMap.SaveBitmapFile(cDC, str(i+1)+".bmp")
     win32gui.DeleteObject(dataBitMap.GetHandle())
+    cDC.DeleteDC()
+    if i != 0:
+        print(i)
+        getaverage(time()-preimage)
+    preimage = time()
+print(time() - starttime)
+print("ffmpeg -y -f image2 -r %d -i %%d.bmp -vcodec libx264 -crf 18 video.mp4" % (int(1/average)))
+os.system("ffmpeg -y -f image2 -r %d -i %%d.bmp -vcodec libx264 -crf 18 video.mp4" % (int(1/average)))
+dcObj.DeleteDC()
+
+win32gui.ReleaseDC(hwnd, wDC)
+
+print("dun")
